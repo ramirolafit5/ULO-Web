96c00f01e18442c0915cdfe7a361b2bb854e0ac8
Ramiro Lafit <ramirolafit5@gmail.com>
2025-06-17 06:54:10 -0300

    SpeedInsights-MetricasVercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import Router from './router.jsx'
 import { createRoot } from 'react-dom/client'; // Asegúrate de importar createRoot de 'react-dom/client'
 import { StrictMode } from 'react';
 import './index.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router />
+    <SpeedInsights />
   </StrictMode>,
 )
