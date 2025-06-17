277fff50c603e23291e0ae1b00510212dd81cf4b
Ramiro Lafit <ramirolafit5@gmail.com>
2025-06-17 07:15:07 -0300

    Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import Router from './router.jsx'
+
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+
 import { createRoot } from 'react-dom/client'; // Asegúrate de importar createRoot de 'react-dom/client'
 import { StrictMode } from 'react';
 import './index.css';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
