fabd531dbc609a5a7c37b423bd559f00f92e1c5d
Ramiro Lafit <ramirolafit5@gmail.com>
2025-06-24 12:03:43 -0300

    performance improvement

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Asegúrate de que este plugin sea el correcto para Tailwind CSS con Vite
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    sourcemap: true, // Agrega esta línea para habilitar los source maps
-    // Puedes usar 'true' para generar archivos .map separados,
-    // o 'hidden' si quieres que se generen pero no se referencien en el código principal (útil para servicios de monitoreo de errores).
-  },
+  plugins: [react(),tailwindcss()],
 })
